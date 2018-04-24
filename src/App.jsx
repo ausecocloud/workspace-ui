64971e2e77ab8423f199b1706713900af80c0aa1
Gerhard Weis <g.weis@griffith.edu.au>
2018-04-24 13:20:15 +1000

    typo

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ class App extends React.Component {
 
   static propTypes = {
     user: PropTypes.objectOf(PropTypes.any),
-    isAuthenticated: PropTypes.boolean,
+    isAuthenticated: PropTypes.bool,
   }
 
   state = {
