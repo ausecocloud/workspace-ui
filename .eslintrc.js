9d45e7f7db9b18b5f5c131d9aa0dc8deb5320dd9
Gerhard Weis <g.weis@griffith.edu.au>
2018-04-26 09:57:54 +1000

    relax some lint rules to warnings in development mode (and for editors as well)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,21 @@
+
+// override rules
+const rules = {
+}
+
+if (process.env.NODE_ENV === 'development' || process.NODE_ENV === undefined) {
+    // relax some rules in development mode
+    rules['no-console'] = 'warn';
+    // disallow declaration of variables that are not used in the code
+    rules['no-unused-vars'] = ['warn', { vars: 'all', args: 'after-used', ignoreRestSiblings: true }];
+    rules['import/first'] = ['warn', 'absolute-first'];
+}
+
 module.exports = {
   "parser": "babel-eslint",
   "extends": "airbnb",
   "env": {
     "browser": true
   },
-  "rules": {
-    "no-console": "warn"
-  }
+  "rules": rules
 };
