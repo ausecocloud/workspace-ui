4d5576fcb9a5fd7cd2e64d20985b802c0edfb58d
Gerhard Weis <g.weis@griffith.edu.au>
2018-05-09 14:50:14 +1000

    use jupyter API tokens instead of oauth tokens. (requires jupyterhub >= 0.9)

--- src/api/jupyterhub.js
+++ src/api/jupyterhub.js
@@ -3,6 +3,9 @@ import { CANCEL } from 'redux-saga';
 import { getClientToken } from './keycloak';
 import { getConfig } from '../config';
 
+// TODO: think about keeping 'username' somewhere global or get it from state tree
+//       instead of passing it down through every API call.
+//       We need username to mint tokens if needed.
 
 let client;
 
@@ -10,8 +13,32 @@ export function getHubUrl() {
   return getConfig('jupyterhub').url;
 }
 
+function getToken(username) {
+  // return jupytrehub API token....
+  return getClientToken(getConfig('jupyterhub').client_id)
+    .then((accessToken) => {
+      const token = localStorage.getItem('jupyterhub_api_token');
+      if (!token) {
+        return axios({
+          url: `hub/api/users/${username}/tokens`,
+          baseURL: getHubUrl(),
+          method: 'POST',
+          data: {
+            auth: { token: accessToken },
+            note: navigator.userAgent,
+          },
+        }).then((resp) => {
+          // apiToken may have: created, id, kind, last_activity, note, token, user
+          localStorage.setItem('jupyterhub_api_token', resp.data.token);
+          return resp.data.token;
+        });
+      }
+      return token;
+    });
+}
+
 // FIXME: almost duplicate code
-function getClient() {
+function getClient(username) {
   if (!client) {
     client = axios.create({
       baseURL: getHubUrl(),
@@ -19,11 +46,11 @@ function getClient() {
     // add auth interceptor
     client.interceptors.request.use(
       // Do something before request is sent
-      config => getClientToken(getConfig('jupyterhub').client_id)
-        .then((token) => {
+      config => getToken(username)
+        .then((apiToken) => {
           const newConfig = config;
-          if (token) {
-            newConfig.headers.Authorization = `Bearer ${token}`;
+          if (apiToken) {
+            newConfig.headers.Authorization = `token ${apiToken}`;
           }
           return newConfig;
         })
@@ -36,20 +63,22 @@ function getClient() {
 }
 
 // FIXME: duplicate code
-function callAPI(options) {
+function callAPI(options, username) {
   // returns a cancelable promise
   const cancel = axios.CancelToken.source();
   const opts = {
     ...options,
     cancelToken: cancel.token,
   };
-  const promise = getClient().request(opts);
+  const promise = getClient(username).request(opts);
+  // TODO: if we get an unauthorized we shoudl try an get a new token (or at least clear the old one in localStorage)
+  //       any other errors we should handle here?
   return { promise, cancel: cancel.cancel };
 }
 
 
 export function getUser(username) {
-  const { promise, cancel } = callAPI({ url: `hub/api/users/${username}` });
+  const { promise, cancel } = callAPI({ url: `hub/api/users/${username}` }, username);
   const data = promise.then(response => response.data);
   data[CANCEL] = cancel;
   return data;
