75a30ea33096aff2b4613c665c1dfb1d151f3942
Samuel Wolski <s.wolski@griffith.edu.au>
2018-05-14 12:11:04 +1000

    Added validation and event handling to user account form.  Still require an action/end-point to post to.

--- src/Account.jsx
+++ src/Account.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Button, Form, FormGroup, Label, Input, Container } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, Container, FormFeedback } from 'reactstrap';
 import { getUser, getAuthenticated } from './reducers';
 
 
@@ -24,28 +24,140 @@ class AccountTable extends React.PureComponent {
     isAuthenticated: PropTypes.bool,
   }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      // form fields
+      name: this.props.user.name,
+      email: this.props.user.email,
+      username: this.props.user.preferred_username,
+      // form state
+      formErrors: { name: '', email: '', username: '' },
+      formState: { name: null, email: null, username: null },
+      // this form is prefilled, valid as true on render
+      formValid: true,
+    };
+  }
+
+  handleChange = (event) => {
+    const { target } = event;
+    const { value, name } = target;
+    const [fieldState, fieldError] = this.validateField(name, value);
+    this.setState({
+      [name]: value,
+      formState: {
+        ...this.state.formState,
+        [name]: fieldState,
+      },
+      formErrors: {
+        ...this.state.formErrors,
+        [name]: fieldError,
+      },
+    }, () => this.validateForm());
+  }
+
+  validateField = (fieldName, value) => {
+    let state = 'is-valid';
+    let errorMsg = '';
+    const emailRegex = new RegExp('[^@]+@[^@]+\\.[^@]+');
+    console.log(fieldName);
+    switch (fieldName) {
+      case 'name':
+        // rule: must be at least 1 char
+        if (value.length <= 0) {
+          state = 'is-invalid';
+          // error for rule
+          errorMsg = 'A users name is required.';
+        }
+        break;
+      case 'email':
+        console.log(value.match(emailRegex));
+        console.log();
+        // rule: must be at least 1 char
+        if (value.length <= 0) {
+          state = 'is-invalid';
+          // error for rule
+          errorMsg = 'An email is required.';
+        } else if (!emailRegex.test(value)) {
+          state = 'is-invalid';
+          // error for rule
+          errorMsg = 'Input must be valid email format (ie.  _____@______.____).';
+        }
+        break;
+      case 'username':
+        console.log(value);
+        // rule: must be at least 1 char
+        if (value.length <= 0) {
+          state = 'is-invalid';
+          // error for rule
+          errorMsg = 'A username is required.';
+        }
+        break;
+      default:
+        break;
+    }
+    return [state, errorMsg];
+  }
+
+  validateForm = () => {
+    console.log('here');
+    Object.entries(this.state.formErrors).forEach(([fieldname, error]) => {
+      if (error.length > 0) {
+        this.setState({ formValid: false });
+      } else {
+        this.setState({ formValid: true });
+      }
+    });
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault();
+    if (this.state.formValid) {
+      const { name, email, username } = this.state;
+      const formData = { name, email };
+      formData.preferred_username = username;
+      console.log(formData);
+      // submit ajax call
+      // action doesn't exist yet
+      // this.props.dispatch(actions.updateUser(formData));
+
+      console.log('form post here');
+    } else {
+      Object.entries(this.state.formErrors).forEach(([fieldname, error]) => {
+        const formState = { ...this.state.formState };
+        formState[fieldname] = 'is-invalid';
+        this.setState({ formState });
+      });
+    }
+  }
+
+
   render() {
     const { isAuthenticated, user } = this.props;
+    const { name, email, username } = this.state;
 
     const userTable = () => (
       <section className="user-details">
         <h1>User details for {user.name}</h1>
-        <Form>
+        <Form onSubmit={this.submitHandler}>
           <FormGroup>
             <Label for="userFullName">Name</Label>
-            <Input type="text" name="user-fullname" id="userFullName" defaultValue={user.name} />
+            <Input type="text" name="name" id="name" defaultValue={name} onChange={this.handleChange} className={this.state.formState.name} />
+            <FormFeedback>{this.state.formErrors.name}</FormFeedback>
           </FormGroup>
           <FormGroup>
             <Label for="userEmail">Email</Label>
-            <Input type="email" name="email" id="userEmail" defaultValue={user.email} />
+            <Input type="email" name="email" id="email" defaultValue={email} onChange={this.handleChange} className={this.state.formState.email} />
+            <FormFeedback>{this.state.formErrors.email}</FormFeedback>
           </FormGroup>
           <FormGroup>
             <Label for="userName">Username</Label>
-            <Input type="text" name="userName" id="userName" defaultValue={user.preferred_username} />
+            <Input type="text" name="username" id="username" defaultValue={username} onChange={this.handleChange} className={this.state.formState.username} />
+            <FormFeedback>{this.state.formErrors.username}</FormFeedback>
           </FormGroup>
           <FormGroup>
             <Label for="userAuth">Identity Issued By</Label>
-            <Input type="text" name="userAuth" id="userAuth" disabled defaultValue={user.iss} />
+            <Input type="text" name="userAuth" id="userAuth" disabled value={user.iss} />
           </FormGroup>
           <Button>Submit</Button>
         </Form>
