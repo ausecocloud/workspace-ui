26a5355542d5c828b3eca37a7db04fb6daca9c84
James Li <j.li7@griffith.edu.au>
2018-08-14 16:10:18 +1000

    Improved rendering of size figures in the interface

--- src/utils.js
+++ src/utils.js
@@ -15,11 +15,27 @@ function formatDate(date) {
   return string;
 }
 
-export
-function bytesToSize(bytes) {
+/**
+ * Generates human readable string representation of given number of bytes
+ *
+ * @param {number} bytes The number of bytes to generate string representation of
+ * @param {boolean} naForZero Whether to display "n/a" for values of 0 (default = true)
+ */
+export function bytesToSize(bytes, naForZero = true) {
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-  if (bytes === 0) return 'n/a';
-  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
-  if (i === 0) return `${bytes} ${sizes[i]})`;
-  return `${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`;
+
+  if (bytes === 0) return naForZero ? 'n/a' : `0 ${sizes[0]}`;
+
+  // Calculate the exponent
+  const exp = Math.log(bytes) / Math.log(1024);
+
+  if (exp < 1) {
+    return `${bytes} ${sizes[0]}`;
+  }
+
+  // Construct the string representation using the closest available exponent
+  const i = (exp >= sizes.length) ? (sizes.length - 1) : Math.floor(exp);
+  const x = exp - i;
+
+  return `${(1024 ** x).toFixed(1)} ${sizes[i]}`;
 }
