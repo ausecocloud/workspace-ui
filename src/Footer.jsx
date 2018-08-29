7a7b5238988c63ae2908e641404ad8c4a52ce553
James Li <j.li7@griffith.edu.au>
2018-08-29 11:57:56 +1000

    Adjusting support links to what's currently available

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -38,13 +38,12 @@ export default class Footer extends React.Component {
               </ul>
             </Col>
             <Col md="3">
-              <h3>Documentation</h3>
-              <ul>
-                <li><a href="#" target="_blank" rel="noopener noreferrer" title="Tutorials">Tutorials</a></li>
-                <li><a href="#" target="_blank" rel="noopener noreferrer" title="Support articles">Support articles</a></li>
-                <li><a href="#" target="_blank" rel="noopener noreferrer" title="FAQ">FAQ</a></li>
-              </ul>
               <h3>Support</h3>
+              <ul>
+                <li><a href="http://support.ecocloud.org.au/support/solutions" target="_blank" rel="noopener noreferrer" title="Documentation">Documentation</a></li>
+                <li><a href="https://www.ecocloud.org.au/contact/" target="_blank" rel="noopener noreferrer" title="Submit a support request">Submit a support request</a></li>
+              </ul>
+              <h3>Get in touch</h3>
               <ul>
                 <li><a href="https://www.ecocloud.org.au/contact/" target="_blank" rel="noopener noreferrer" title="Contact us">Contact us</a></li>
               </ul>
