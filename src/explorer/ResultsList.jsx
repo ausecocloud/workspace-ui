7621a3b5be11f6dce4a95708e8a204827ab730c3
James Li <j.li7@griffith.edu.au>
2018-09-25 11:19:15 +1000

    Removing direct links to distribution files from Explorer

--- src/explorer/ResultsList.jsx
+++ src/explorer/ResultsList.jsx
@@ -68,8 +68,10 @@ class ResultsList extends React.Component {
             const url = dist.downloadURL || dist.accessURL;
             return (
               <li key={dist.identifier}>
-                <input type="checkbox" checked={this.props.selectedDistributions.has(dist.identifier)} onChange={e => this.handleCheckboxChange(dist, e.target.checked)} />
-                {' '}<a href={url}>{dist.title}</a>{' '}
+                <label>
+                  <input type="checkbox" checked={this.props.selectedDistributions.has(dist.identifier)} onChange={e => this.handleCheckboxChange(dist, e.target.checked)} />
+                  <span class="title">{dist.title}</span>
+                </label>
                 <small className="licence-header"> Format </small>
                 <small className="format">{dist.format}</small>
                 <i className="licence-hover" id={`dist-${ridx}-${didx}`}> <small className="licence-header">  Licence </small>
